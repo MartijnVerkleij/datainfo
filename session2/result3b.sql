62d3b61b40cf2ab637b2b3cec51ad10c4c429dc9
MartijnVerkleij <mail@mverkleij.nl>
2014-05-06 17:06:12 +0200

    Shunting Rule better

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,8 +1,7 @@
 SELECT p.pid FROM Person p 
 WHERE EXISTS( 
-	SELECT * DISTINCT FROM Acts a, Movie m 
+	SELECT DISTINCT * FROM Acts a, Movie m 
 	WHERE a.pid = p.pid 
 	AND m.mid = a.mid
 	AND m.name = 'Back to the Future' 
-	) 
 );  
