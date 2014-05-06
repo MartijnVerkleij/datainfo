349d8a7dded842d276224c62c7384bf32348ca50
MartijnVerkleij <mail@mverkleij.nl>
2014-05-06 16:20:15 +0200

    Sessie 2 Opgave 2 taak 2

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,1 +1,5 @@
 INSERT INTO F (x,y,violation,motivation) VALUES ('ABC','EF',FALSE,'Kan alles bereiken, want E-->D'),('E','D',TRUE,'Alleen E,D,A liggen in scope'),('D','A',TRUE,'Alleen D,A liggen in scope');
+
+INSERT INTO F1 (x,y,violation,motivation) VALUES ('E','D',FALSE,'Superkey'),('D','A',TRUE,'E niet bereikbaar');
+INSERT INTO F2 (x,y,violation,motivation) VALUES ('ABC','F',FALSE,'Kan alles bereiken');
+
