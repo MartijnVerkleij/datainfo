e1b6c038faf21b136f72768557ca06cd19a592f4
MartijnVerkleij <mail@mverkleij.nl>
2014-05-27 14:39:00 +0200

    opgave 1f

---
+++ session5/excercise1f.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Boek (
+    isbn varchar(13) PRIMARY KEY,
+    titel varchar(100),
+    auteur varchar(100)
+);
+
+CREATE TABLE Exemplaar(
+    isbn varchar(13) REFERENCES Boek(isbn) 
+        ON UPDATE CASCADE
+        INITIALLY DEFERRED NOT DEFERRABLE,
+    volgnummer integer,
+    gewicht integer,
+    kast varchar(50),
+    PRIMARY KEY (isbn, volgnummer)
+);
+
+CREATE TRIGGER exemplaar 
+    AFTER DELETE ON Boek B 
+    DEFERRABLE INITIALLY IMMEDIATE
+    FOR EACH ROW 
+    EXECUTE PROCEDURE 
+        DELETE * FROM Exemplaar E
+        WHERE E.isbn = B.isbn;
